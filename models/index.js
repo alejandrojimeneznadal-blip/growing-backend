aa75e5a6848afc976cf9581a344fad239ded609b
alejandrojimeneznadal-blip <alejandrojimeneznadal@gmail.com>
2025-12-03 17:39:19 +0100

    Add Recurso model to index.js exports

--- models/index.js
+++ models/index.js
@@ -2,23 +2,21 @@ const sequelize = require('../config/database');
 const User = require('./User');
 const Conversation = require('./Conversation');
 const Message = require('./Message');
+const Recurso = require('./Recurso');
 
 // Define relationships
 User.hasMany(Conversation, {
   foreignKey: 'userId',
   as: 'conversations'
 });
-
 Conversation.belongsTo(User, {
   foreignKey: 'userId',
   as: 'user'
 });
-
 Conversation.hasMany(Message, {
   foreignKey: 'conversationId',
   as: 'messages'
 });
-
 Message.belongsTo(Conversation, {
   foreignKey: 'conversationId',
   as: 'conversation'
@@ -28,5 +26,6 @@ module.exports = {
   sequelize,
   User,
   Conversation,
-  Message
+  Message,
+  Recurso
 };
